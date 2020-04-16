344f0cab86a4f3837488206a153301969c327374
igorgbr <igorgphotoarte@gmail.com>
2020-04-16 00:31:38 -0300

    exercicios form inciando javascript no form

---
+++ exercises/6_1/script.js
@@ -0,0 +1,10 @@
+let inputEstados = document.querySelector('#estados');
+
+let estados = ['AC', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG',
+'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
+
+for(let i = 0; i < estados.length; i+=1){
+    let opcao = document.createElement('option');
+    opcao.innerHTML = estados[i];
+    inputEstados.appendChild(opcao);
+};
