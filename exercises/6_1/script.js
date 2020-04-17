8566443a4cf11e15aae8f75de5800ba0eca506bc
igorgbr <igorgphotoarte@gmail.com>
2020-04-16 23:43:54 -0300

    alteraçoes realizadas no form

--- exercises/6_1/script.js
+++ exercises/6_1/script.js
@@ -1,10 +1,10 @@
 let inputEstados = document.querySelector('#estados');
-let inputData = document.querySelector('#data-inicio');
+let inputData = document.querySelector('#data-inicio').DatePickerX.init('yyyy/mm/dd');
 
 let estados = ['AC', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG',
 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
 
-for(let i = 0; i < estados.length; i+=1){
+for(let i = 0; i < estados.length; i+=1) {
     let opcao = document.createElement('option');
     opcao.innerHTML = estados[i];
     inputEstados.appendChild(opcao);
