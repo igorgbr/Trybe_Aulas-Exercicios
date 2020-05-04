3166a9f4b28d3de8eeda0d1d4e10d001b6406f6d
igorgbr <igorgphotoarte@gmail.com>
2020-05-04 19:16:58 -0300

    exercicio 1 ok

---
+++ exercises/8_2/exercicio1.js
@@ -0,0 +1,30 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+  const morningTurn = (novoObjeto, newKey, value) => {
+      novoObjeto[newKey] = value;
+      return novoObjeto;
+
+  }
+
+const newKey = 'turno';
+const newValue = 'manhã';
+
+  console.log(morningTurn(lesson2, newKey, newValue))
