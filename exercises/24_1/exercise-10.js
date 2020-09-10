7b67b582b3eb3faf75809e21cabf73f5be20772f
igorgbr <igorgphotoarte@gmail.com>
2020-09-09 22:22:42 -0300

    all

--- exercises/24_1/exercise-10.js
+++ exercises/24_1/exercise-10.js
@@ -1,5 +1,9 @@
 db.movies.find({ title: 'Home Alone' });
 db.movies.updateOne(
   { title: 'Home Alone' },
-  { $rename: { budget: 'estimatedBudget' } }
+  {
+    $currentDate: {
+      lastUpdate: { $type: 'timestamp' },
+    },
+  }
 );
--- exercises/24_1/exercise-8.js
+++ exercises/24_1/exercise-8.js
@@ -1,5 +1,5 @@
 db.movies.find({ title: 'Home Alone' });
 db.movies.updateOne(
   { title: 'Home Alone' },
-  { $rename: { budget: 'estimatedBudget' } }
+  { $min: { budget: 5} }
 );
