84557627c463d7e0933f3f5928c9838495367741
igorgbr <igorgphotoarte@gmail.com>
2020-09-12 01:38:49 -0300

    ate o exercicio 15

---
+++ exercises/24_3/exerc-14.js
@@ -0,0 +1,10 @@
+//Retorne os filmes em que o ratings possua tamanho 4 e que seja da category "adventure" ou "family",
+//mas que não tenha o imdbRating menor que 7.
+db.movies.find(
+  {
+    category: { $in: ['adventure', 'family'] },
+    ratings: {$size: 4},
+    imdbRating: {$gt: 7}
+  },
+  { id: 0 }
+);
