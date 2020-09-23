70d068e24b7d3656c6635e64acbf04b2a57b3060
igorgbr <igorgphotoarte@gmail.com>
2020-09-22 21:28:25 -0300

    exercicio 2

--- exercises/26_2/exercicio 1/index.js
+++ exercises/26_2/exercicio 1/index.js
@@ -1,4 +1,5 @@
-function validaNumeros(a, b, c) {
+
+/* function validaNumeros(a, b, c) {
   return new Promise((resolve, reject) => {
     if (isNaN(a) || isNaN(b) || isNaN(c)) reject('digite um numero');
 
@@ -14,5 +15,16 @@ function validaNumeros(a, b, c) {
     .catch((err) => console.error(err));
 }
 
-validaNumeros(50, 62, 23);
-/* .catch(console.error); */
+validaNumeros(50, 62, 23); */
+
+const validaNumeros = async (a, b, c) => {
+    if (isNaN(a) || isNaN(b) || isNaN(c)) Promise.reject('digite um numero')
+
+    const sum = a + b;
+    const mul = sum * c;
+    return mul < 50 ? Promise.reject('valor muito baixo') : console.log(mul);
+};
+
+
+validaNumeros(51, 80, 54)
+.catch((err) => console.error(err))
