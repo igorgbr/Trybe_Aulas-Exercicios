0455d3f32fab77bb5595fcee9d63a2260a640ca6
igorgbr <igorgphotoarte@gmail.com>
2020-09-29 16:46:15 -0300

    treinamento

---
+++ exercises/treinamento_promises/withAPI/asyncAwait/letras.js
@@ -0,0 +1,43 @@
+function findLyrics(artist, song) {
+  return fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`);
+}
+
+const form = document.querySelector('#lyrics_form');
+form.addEventListener('submit', (el) => {
+  el.preventDefault();
+  doSubmit();
+});
+
+async function doSubmit() {
+  const lyrics_el = document.querySelector('#lyrics');
+  const artist = document.querySelector('#artist');
+  const song = document.querySelector('#song');
+
+  lyrics_el.innerHTML =
+    '<div class="spinner-grow" role="status"><span class="sr-only">Carregando...</span></div>';
+
+  /*   findLyrics(artist.value, song.value)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.lyrics) {
+        lyrics_el.innerHTML = data.lyrics;
+      } else {
+        lyrics_el.innerHTML = data.error;
+      }
+    })
+    .catch((error) => {
+      lyrics_el.innerHTML = `Ooops ${error}`;
+    }); */
+  try {
+    const lyricsresponse = await findLyrics(artist.value, song.value);
+    const data = await lyricsresponse.json();
+    
+    if (data.lyrics) {
+      lyrics_el.innerHTML = data.lyrics;
+    } else {
+      lyrics_el.innerHTML = data.error;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
