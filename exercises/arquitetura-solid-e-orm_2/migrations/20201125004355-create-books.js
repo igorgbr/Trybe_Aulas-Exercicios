ca52f95d1087a57f6bb43bd2a30488d32d4001c4
igorgbr <igorgphotoarte@gmail.com>
2020-11-25 00:09:25 -0300

    primeira parte - model e sequelize criado

---
+++ exercises/arquitetura-solid-e-orm_2/migrations/20201125004355-create-books.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const BooksTable = queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      author: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      pageQuantity: {
+        allowNull: true,
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+
+    return BooksTable;
+  },
+
+  down: async (queryInterface, Sequelize) => queryInterface.dropTable('Books'),
+};
